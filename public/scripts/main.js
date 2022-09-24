f56994cb798cc8664ab72373e75362ebe502a78b
Osmael Sousa <osmaelsousa.ti@gmail.com>
2022-09-24 19:26:11 -0300

    modificando informações de modal, de acordo com o button clicado

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -3,22 +3,36 @@ import Modal from './modal.js'
 
 const modal = Modal()
 
+const modalTitle = document.querySelector('.modal h2')
+const modalDescription = document.querySelector('.modal p')
+const modalButton = document.querySelector('.modal button')
+
 /* abrindo a modal quando o botão marcado como lida for clicado */
 const checkButtons = document.querySelectorAll('.actions a.check');
 
 checkButtons.forEach( button => {
-    
-    button.addEventListener('click', event => {
-        modal.open()
-    })
-
+    button.addEventListener('click', handleClick)
 })
 
 /* abrindo a modal quando o botão excluir for clicado */
 const deleteButtons = document.querySelectorAll('.actions a.delete')
 
 deleteButtons.forEach( button => {
-    button.addEventListener('click', event => {
-        modal.open()
-    })
+    button.addEventListener('click', (event) => handleClick(event, false))
 })
+
+function handleClick(event, check = true) {
+
+    const text = check ? 'Marcar como lida' : 'Excluir'
+    const question = 'Tem certeza que deseja'
+
+    modalTitle.innerHTML = `${text} esta pergunta`
+    modalDescription.innerHTML = check ? `${question} marcar como lida esta pergunta?` : `${question} excluir esta pergunta?`
+    modalButton.innerHTML = `Sim, ${text.toLowerCase()}`
+
+    // adicionando ou removendo classe .red
+    check ? modalButton.classList.remove('red') : modalButton.classList.add('red')
+
+    // abrir modal
+    modal.open()
+}
