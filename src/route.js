b5b7efff25aa3ebf5cda92e91d4083053d1b5945
Osmael Sousa <osmaelsousa.ti@gmail.com>
2022-09-30 19:31:31 -0300

    renderizando html de forma dinâmica com ejs

--- src/route.js
+++ src/route.js
@@ -9,9 +9,10 @@ const questionController = require('./controllers/QuestionController')
 const route = express.Router()
 
 // definindo as rotas
-route.get('/', (req, res) => {res.render('index')})
+route.get('/', (req, res) => {res.render('index', {page: 'enter-room'})})
+route.get('/create-pass', (req, res) => {res.render('index', {page: 'create-pass'})})
+
 route.get('/room', (req, res) => {res.render('room')})
-route.get('/create-pass', (req, res) => {res.render('create-pass')})
 
 /*  quando colocamos : da forma abaixo, estamos falando
     para o express que o conteúdo que vai vim é desconhecido
