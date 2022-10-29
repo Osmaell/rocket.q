6d42fe23c27adbab3e14ecd096d46ff25d7e905a
Osmael Sousa <osmaelsousa.ti@gmail.com>
2022-10-29 11:24:17 -0300

    validando se roomID já existe no banco de dados

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -7,20 +7,34 @@ module.exports = {
         const db = await Database();
         const pass = req.body.password;
         
-        let roomId;
-        
-        for(var i = 0; i < 6; i++) {
-            i == 0 ? roomId = Math.floor(Math.random() * 10).toString() : 
-                     roomId += Math.floor(Math.random() * 10).toString();
+        let roomId
+        let isRoom = true
+
+        while(isRoom) {
+
+            // gerando o número da room
+            for(var i = 0; i < 6; i++) {
+                i == 0 ? roomId = Math.floor(Math.random() * 10).toString() : 
+                         roomId += Math.floor(Math.random() * 10).toString();
+            }
+
+            // verificando se número da room já existe
+            const roomsExistIds = await db.all(`SELECT id FROM rooms`);
+            isRoom = roomsExistIds.some(roomExistId => roomExistId === roomId);
+
+            if (!isRoom) {
+
+                // inserindo a room no banco de dados
+                await db.run(`INSERT INTO rooms (id, pass)
+                    VALUES (
+                        ${parseInt(roomId)},
+                        ${pass}
+                    )`);
+    
+            }
+
         }
 
-        // inserindo no banco de dados
-        await db.run(`INSERT INTO rooms (id, pass)
-            VALUES (
-                ${parseInt(roomId)},
-                ${pass}
-            )`);
-        
         await db.close();
         
         res.redirect(`/room/${roomId}`);
